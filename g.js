06cf9edafb2c924ec551f359c469716e4da38b27
Muslimprogrammer292 mail.com <abdulhameed.kkhan1@gmail.com>
2025-03-02 10:01:01 +0600

    I finished everything in index.html

---
+++ g.js
@@ -0,0 +1,7 @@
+
+    let red = Math.floor(Math.random()*256);
+    let green = Math.floor(Math.random()*256);
+    let blue = Math.floor(Math.random()*256);
+    let randomColor = 'bg-[rgb('+red+', '+green+', '+blue+')]';
+    randomColorString = randomColor.toString();
+    console.log(randomColorString);
